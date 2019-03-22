8d1726e80988efd3667e4df4564d8bdcf9ede89c
Sean Feldman <feldman.sean@gmail.com>
2019-03-21 20:49:43 -0700

    Remove Task.Factory.New

--- AppInsightsWithWebJob/ContinuousJob.cs
+++ AppInsightsWithWebJob/ContinuousJob.cs
@@ -15,25 +15,20 @@ namespace AppInsightsWithWebJob
             this.logger = logger;
         }
 
-        public Task StartAsync(CancellationToken cancellationToken)
+        public async Task StartAsync(CancellationToken cancellationToken)
         {
-            Task.Factory.StartNew(async () =>
+            var counter = 0;
+            while (!cancellationToken.IsCancellationRequested)
             {
-                var counter = 0;
-                while (!cancellationToken.IsCancellationRequested)
+                Console.Write(".");
+                await Task.Delay(100, cancellationToken);
+
+                if (counter++ > 20)
                 {
-                    Console.Write(".");
-                    await Task.Delay(100, cancellationToken);
-
-                    if (counter++ > 20)
-                    {
-                        logger.LogInformation("<<< About to throw >>>");
-                        throw new InvalidOperationException("oy vay!");
-                    }
+                    logger.LogInformation("!!! About to throw !!!");
+                    throw new InvalidOperationException("oy vay!");
                 }
-            },TaskCreationOptions.LongRunning);
-
-            return Task.CompletedTask;
+            }
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
